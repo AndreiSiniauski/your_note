3327f41ad39c29b0c1b01ca61928f2a5a78be4ed
Andry <79461163+AndreiSiniauski@users.noreply.github.com>
2024-07-05 13:50:48 +0000

    add username

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    if(window.Telegram.WebApp.initDataUnsafe && window.Telegram.WebApp.initDataUnsafe.user) {
+      setUsername(window.Telegram.WebApp.initDataUnsafe.user.username)
+    }
+
+    window.Telegram.WebApp.expand();
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <header className='App-header'>
+        <p>Добро пожаловать, <strong>{username || 'гость'}</strong>!</p>
+      </header>
+    </div>
   );
 }
 
